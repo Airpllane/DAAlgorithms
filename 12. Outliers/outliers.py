232264548d0780f5ed7a3e8c25c0035ff3a8ce7f
Airplane <letadllo@gmail.com>
2021-03-18 16:40:42 +0500

    Outliers

--- 12. Outliers/outliers.py
+++ 12. Outliers/outliers.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 
 from sklearn.decomposition import PCA
 from sklearn.covariance import EmpiricalCovariance
+from pyod.models.hbos import HBOS
+
+import warnings
+warnings.filterwarnings("ignore", category = UserWarning)
 
 #%% Load data
 
@@ -15,7 +19,7 @@ del data2['conterfeit']
 
 #%% Process data
 
-X = data1
+X = data2
 pca = PCA(n_components = 1)
 X = pca.fit_transform(X)
 
@@ -25,7 +29,29 @@ plt.figure(figsize = (20, 2))
 plt.plot(X, np.zeros_like(X) + 0, 'x')
 plt.show()
 
-#%% Execute
+#%% Execute, plot
 
 clf = EmpiricalCovariance()
 clf.fit(X)
+clfc = clf.score(X)
+clf2 = HBOS()
+clf2.fit(X)
+clf2c = clf2.decision_function(X).mean()
+
+fig, ax = plt.subplots(2, figsize = (12, 4))
+for x in X:
+    ax[0].plot(x, 0, 'bx' if clf.score(x) > clfc else 'rx')
+    ax[0].set_title('Max likelihood')
+    ax[0].set(xlabel = 'PCA feature')
+    ax[0].set_yticks([])
+    ax[1].plot(x, 0, 'bx' if clf2.decision_function(np.reshape(x, (-1, 1))) < clf2c else 'rx')
+    ax[1].set_title('Histogram-based')
+    ax[1].set(xlabel = 'PCA feature')
+    ax[1].set_yticks([])
+plt.tight_layout()
+plt.show()
+
+#%%
+
+
+
